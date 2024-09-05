9db22397bca42cdfaeae1b5f18f532ca9e0e2e8a
sabbirsami <smd71430@gmail.com>
2024-09-05 18:01:17 +0600

    background change option done

--- src/components/page/settings/Settings.tsx
+++ src/components/page/settings/Settings.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useBackground } from "../../context/BackgroundContext";
 
 const Settings: React.FC = () => {
-  return <div>Setting</div>;
+  const { backgrounds, selectBackground, displayedBackground } =
+    useBackground();
+  return (
+    <div className="">
+      <div className="grid grid-cols-9">
+        <div className="col-span-4"></div>
+        <div className="col-span-3"></div>
+        <div className="col-span-2">
+          <div className="h-full bg-black/35 backdrop-blur-[180px] p-6 rounded-md h-[96vh] overflow-y-auto">
+            <div className="mb-4">
+              <p className="text-sm pb-1">Selected Background</p>
+              <img src={displayedBackground} className="w-full h-32" alt="" />
+            </div>
+            <p className="text-sm pb-1">Choose Background</p>
+            <div className="grid grid-cols-2 gap-2.5">
+              {backgrounds.map((bg, index) => (
+                <img
+                  key={index}
+                  src={bg}
+                  alt={`Background ${index + 1}`}
+                  className="rounded-md"
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  onClick={() => selectBackground(bg)}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;

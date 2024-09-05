560ca51885f561a083a7909918f492d373517708
sabbirsami <smd71430@gmail.com>
2024-09-05 15:30:13 +0600

    change favicon

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,8 +1,17 @@
+import useGetDailyRandomBackground from "../components/hooks/useGetRandomBackground";
 import { Button } from "../components/ui/button";
 
 const DashboardLayout: React.FC = () => {
+  const dailyBackground = useGetDailyRandomBackground();
   return (
-    <div>
+    <div
+      className="min-h-screen w-screen"
+      style={{
+        backgroundImage: `url("${dailyBackground}")`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <p>Navbar</p>
       <Button variant="outline">Button</Button>
     </div>

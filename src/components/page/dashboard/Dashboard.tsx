b994b170f90f6562f7ece44b7e018c38f388d33e
Sayemsaadat0 <sayemsaadat0@gmail.com>
2024-09-26 09:46:58 +0600

    s

--- src/components/page/dashboard/Dashboard.tsx
+++ src/components/page/dashboard/Dashboard.tsx
@@ -1,8 +1,21 @@
+import Marquee from "react-fast-marquee";
 
 const Dashboard = () => {
     return (
-        <div className="">Hello </div>
-    )
-}
+        <div>
+            <Marquee>
+                {[...new Array(10)].map((_, i) => (
+                    <div key={i} className="marquee-item">
+                        <img
+                            className="h-20 object-cover"
+                            src="https://i.pinimg.com/564x/e7/e9/9d/e7e99de3fcbf96d1dfda430102ceb662.jpg"
+                            alt=""
+                        />
+                    </div>
+                ))}
+            </Marquee>
+        </div>
+    );
+};
 
-export default Dashboard
+export default Dashboard;
